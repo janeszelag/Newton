930cc953536fe2d4fb3d776228131d9009649bcc
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-03-17 21:50:47 -0400

    tried autoresponsive but not working

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,63 +1,74 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import IconButton from "@material-ui/core/IconButton";
-import Pin from "./Pin"
+import Pin from "./Pin";
 import styled from "styled-components";
 
+import AutoResponsive from 'autoresponsive-react';
 
-
-const MainDiv = styled.div`
-  width: 100%;
-  margin-top: 2em;
-
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledGridList = styled(GridList)`
-width: 80%;
-height 100%;
-`
-const StyledGridListTile = styled(GridListTile)`
-border-radius: 30px;
-`
-
-
-
-const useStyles = makeStyles(theme => ({
- 
-  gridList: {
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: "translateZ(0)"
-  },
-  titleBar: {
-    background: "#a6b07e",
-    fontWeight: 600
-      // "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
-      // "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
-  },
-  icon: {
-    color: "#c97064"
-  }
-}));
+const getWidth = () =>
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
 
 export default function Menu(props) {
-  const classes = useStyles();
-
-  const [isMobile, setIsMobile] = useState(false);
+  let [width, setWidth] = useState(getWidth());
 
   useEffect(() => {
-    if (window.innerWidth < 600) setIsMobile(true);
-  }, []);
+    const resizeListener = () => {
+      // change width from the state object
+      setWidth(getWidth())
+    };
+    // set resize listener
+    window.addEventListener('resize', resizeListener);
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener);
+    }
+  }, [])
+
+
+  const getAutoResponsiveProps = () => {
+    return {
+      itemMargin: 10,
+      containerWidth: width || document.body.clientWidth,
+      itemClassName: 'item',
+      gridWidth: 100,
+      transitionDuration: '.5'
+    };
+  }
 
   return (
-  
-   
+    <div className="albumPanel">
+    <AutoResponsive  {...getAutoResponsiveProps()}>
+      {
+        props.resources.map((i, index) => {
+          let style = {
+            width: i.w === 'w1' ? 190 : 390,
+            height: i.w === 'w1' ? 240 : 490
+          };
+          return (
+            <a key={index} href="#" className={`${i.w} album item`} style={style}>
+              <img className="a-cont j_ACont" src="images/a.jpg"/>
+              <img className="a-cover" src={i.img_url}/>
+              <p className="a-mask">{index}<i></i></p>
+              <p className="a-layer">
+              
+                <span className="al-title">{i.title}</span>
+               
+              </p>
+              <p className="a-more j_ALMore"></p>
+            </a>
+          );
+        })
+      }
+    </AutoResponsive>
+  </div>
   );
 }
