dec5c900d03fccffd35d0dc78ed9941910422cb1
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-03-31 18:59:51 -0400

    img styled, border styled

--- client/src/components/Resource.js
+++ client/src/components/Resource.js
@@ -1,48 +1,55 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Paper from "@material-ui/core/Paper";
-import {
-  useParams
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 const axios = require("axios").default;
-
 
 //will need to grab comments in addition to resource
 // needs save pin
-// back button 
+// back button
 // link button - could be title
 // rounded paper div, or rounded paper like pinterest would be nice
 
 export const MainDiv = styled.div`
   width: 100%;
-  margin-top: 2em;
+  height: 100vh;
+  margin-top: 4em;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  align-items: center;
 `;
 
+const StyledGrid = styled(Grid)`
+  width: 100%;
+  height: 100%;
+`;
 
-const StyledPaper = styled(Paper)`
-width: 80%;
-display: flex;
-flex-wrap: wrap;
-justify-content: center;
-align-items: center;
-`
-const ImgDiv = styled.div`
-width: 50%;
-`
-const CommentsDiv = styled.div`
-width: 50%;
-`
+const ContentDiv = styled.div`
+  width: 80%;
+  height: 75%;
+  
+  
+  border-color: #808080;
+  border-radius: 10px;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3);
+`;
+const GridItem = styled(Grid)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
 const StyledImg = styled.img`
-height: 70vh;
-  width: 70vh;
-`
-const CommentsPaper = styled(Paper)`
+  height: 100%;
+  width: 100%;
 
-`
+  border-radius: 10px 0px 0px 10px;
+`;
+
+const CommentsPaper = styled(Paper)`
+width: 90%;
+`;
 
 export default function Resource() {
   const [resource, setResource] = useState([]);
@@ -64,7 +71,7 @@ export default function Resource() {
         withCredentials: false
       })
       .then(response => {
-        console.log(response.data)
+        console.log(response.data);
         setResource(response.data);
       })
       .catch(function(error) {
@@ -72,25 +79,24 @@ export default function Resource() {
       });
   }, []);
 
- 
   return (
     <main>
-       <MainDiv>
-       <StyledPaper>
-         
-         <ImgDiv>
-         <StyledImg src={resource.img_url} alt={resource.title}/>
-         </ImgDiv>
-         <CommentsDiv>
-           <CommentsPaper>
-           <h1>{resource.title}</h1>
-           </CommentsPaper>
-        
-         </CommentsDiv>
-       </StyledPaper>
-
-       </MainDiv>
-      
+      <MainDiv>
+        <ContentDiv>
+          <StyledGrid container spacing={0}>
+            <GridItem item xs={12} sm={12} lg={6}>
+              <StyledImg src={resource.img_url} alt={resource.title} />
+            </GridItem>
+            <Grid item xs={12} sm={6}>
+              <GridItem>
+                <CommentsPaper>
+                  <h1>{resource.title}</h1>
+                </CommentsPaper>
+              </GridItem>
+            </Grid>
+          </StyledGrid>
+        </ContentDiv>
+      </MainDiv>
     </main>
   );
 }
