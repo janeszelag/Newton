682fc44c2a5935a9865a3a8b86252d45cb21e3ea
Katherine Szelag <katherine.szelag@mail.utoronto.ca>
2020-04-21 18:06:50 -0400

    prviate routes now, root also redirects to login or menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import Nav from "./components/Nav";
 import About from "./components/About";
@@ -14,13 +14,15 @@ function App() {
     state,
     setUserCookie,
     setUser,
-    removeUserCookie
+    removeUserCookie,
+    authenticatetUser
   } = useApplicationData();
 
   const links = [
     {
       name: "Signup",
       path: "/signup",
+      requiresAuthentication: false,
       component: (
         <Signup
           name={state.user.firstName}
@@ -34,26 +36,48 @@ function App() {
     {
       name: "Login",
       path: "/login",
+      requiresAuthentication: false,
       component: <Login setUserCookie={setUserCookie} setUser={setUser} />
     },
     {
       name: "Menu",
       path: "/menu",
+      requiresAuthentication: true,
       component: <Menu id={state.user.id} />
     },
     {
       name: "About",
       path: "/about",
+      requiresAuthentication: false,
       component: <About />
     }
   ];
 
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        authenticatetUser() ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to="/login" />
+          )
+      }
+    />
+  );
+
   const routes = links.map((link, index) => {
-    return (
-      <Route key={index} path={link.path}>
-        {link.component}
-      </Route>
-    );
+    return link.requiresAuthentication ? (
+      <PrivateRoute
+        key={index}
+        path={link.path}
+        component={() => link.component}
+      />
+    ) : (
+        <Route key={index} path={link.path}>
+          {link.component}
+        </Route>
+      );
   });
 
   return (
@@ -66,6 +90,13 @@ function App() {
           setUser={setUser}
         />
         <Switch>
+        <Route exact path="/">
+            {authenticatetUser() ? (
+              <Redirect to="/menu" id={state.user.id} />
+            ) : (
+              <Login setUserCookie={setUserCookie} setUser={setUser} />
+              )}
+          </Route>
           <Route path="/resources/:id">
             {" "}
             <Resource />
